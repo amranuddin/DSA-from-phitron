2617fb5c6698104388272356d4a2cf7e1d93c7e2
Amran uddin <amranmetrouni@gmail.com>
2025-07-29 12:43:20 -0700

    Add files via upload

---
+++ Singly link list/Singly operation 2/4Head_delete.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_head(Node *&head, int val)
+{
+    Node *newnode = new Node(val);
+    newnode->next = head;
+    head = newnode;
+}
+void print_linklst(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
+}
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    tail = tail->next;
+}
+void reverse_print(Node *tmp)
+{
+    // base case
+    if (tmp == NULL)
+    {
+        return;
+    }
+    reverse_print(tmp->next);
+    cout << tmp->val << endl;
+}
+
+void delete_at_head(Node *&head)
+{
+    Node *deletenode = head;
+    head = head->next;
+    delete deletenode;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+
+    delete_at_head(head);
+    print_linklst(head);
+
+    return 0;
+}
